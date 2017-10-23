836f636a2b5186321f2b9a54666ce54ff498eeca
Valentin Burger <valliburger@gmail.com>
2017-10-23 16:48:10 +0000

    run.sh updated

--- run.sh
+++ run.sh
@@ -5,45 +5,11 @@
 #log_output_dir="/home/valli/workspace/HASDocker/logs/BBB/"
 log_output_dir=$(pwd)"/logs/"
 
-#read -p "Reps? (>0): " reps
-
-#if [ $reps -lt 1 ]
-#then
-#	echo "Invalid repetition number."
-#	exit 1
-#fi
-
-#read -p "BW? (kbps): " bw
-#
-#if [ $bw -lt 1 ]
-#then
-#        echo "Invalid bandwidth."
-#        exit 1
-#fi
-
 #start server
 killall python
 sleep 2s
 #current_dir=$PWD;
 #cd ../..;
-#python -m SimpleHTTPServer &> server.log &
-#cd $current_dir;
-
-#mkdir $log_output_dir
-
-# Delete old logs and create new empty one
-#sudo touch $browser_log_empty_dir
-
-# Make sure scripts are executable
-#for trace in "$traces_dir"/report_*.sh ; do
-#	chmod +x $trace
-#done
-
-#if ! pgrep -x "chromium" > /dev/null
-#then
-#	pgrep chromium | xargs kill -INT
-#	sleep 3s
-#fi
 #reps=20
 bw=$1;
 cvar=$2;
@@ -85,39 +51,8 @@ reps=$4;
 			python tapas/play.py -u http://127.0.0.1:8000/has-evalvm/vids/Parkour/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8 -m nodec -i $3 -b $bwparam > player.log
 #			python tapas-master/play.py -u http://localhost:8000/vids/BBB/playlist.m3u8 -m nodec -b $bwparam > player.log
 
-			# Wait for player to start video playback
-#			loopStopper=0
-#			while [ $loopStopper = 0 ]; do
-#		
-#				s=$($funcs_dir/check_playback_started.sh)
-#				if [ $s = "started" ]; then
-#					loopStopper=1
-#					#$funcs_dir/trace_loop.sh $trace &
-#					# echo "Bandwidth shaping started"
-#				fi
-#			done
-
-
-			# Check every 1s if video playback has finished
-#			loopStopper=0
-#			while [ $loopStopper = 0 ]; do
-#		
-#				s=$($funcs_dir/check_playback_completed.sh)
-#		
-#				if [ $s = "complete" ]; then
-#					cp $browser_log_dir $output
-#					#cp $browser_log_empty_dir  $browser_log_dir
-#					cp ../../server.log "$direct"/server_"$bw"kbit_"$counter".log
-
-#					loopStopper=1
-#					pgrep chromium | xargs kill -INT
-#				fi
-
-#				sleep 1s
-#			done
-
-		      cp server.log "$direct"/server_"$bw"kbit_cv"$cvar"_"$counter".log	
-		      cp player.log "$direct"/player_"$bw"kbit_cv"$cvar"_"$counter".log
+		      cp server.log "$direct"/server_"$bw"kbit_cv"$cvar"_init"$3"_"$counter".log	
+		      cp player.log "$direct"/player_"$bw"kbit_cv"$cvar"_init"$3"_"$counter".log
 
 #			pgrep trace_loop | xargs kill
 #			pgrep report_* | xargs kill
@@ -129,3 +64,6 @@ reps=$4;
 		done
 #	done
 #done
+
+tar czf player_"$bw"kbit_cv"$cvar"_init"$3".tar.gz logs/*.log
+scp -o -o StrictHostKeyChecking=no player_"$bw"kbit_cv"$cvar"_init"$3".tar.gz valli@132.187.12.137:workspace/HASDocker/
